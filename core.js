a1577708acc29066ebd740cb227ad2a16f89a6fd
dautruongybb <30793156+dautruongybb@users.noreply.github.com>
2024-12-29 13:54:49 +0700

    Update core.js

--- core.js
+++ core.js
@@ -19,24 +19,6 @@ var gu = 0;
 // Alphabetical order
 var available_langs = {
     "vn_vn": { "name": "Tiếng Việt", "file": "vn_vn.json", "direction": "ltr"},
-    "ar_ar": { "name": "العربية", "file": "ar_ar.json", "direction": "rtl"},
-    "bg_bg": { "name": "Български", "file": "bg_bg.json", "direction": "ltr"},
-    "cz_cz": { "name": "Čeština", "file": "cz_cz.json", "direction": "ltr"},
-    "de_de": { "name": "Deutsch", "file": "de_de.json", "direction": "ltr"},
-    "es_es": { "name": "Español", "file": "es_es.json", "direction": "ltr"},
-    "fr_fr": { "name": "Français", "file": "fr_fr.json", "direction": "ltr"},
-    "hu_hu": { "name": "Magyar", "file": "hu_hu.json", "direction": "ltr"},
-    "it_it": { "name": "Italiano", "file": "it_it.json", "direction": "ltr"},
-    "jp_jp": { "name": "日本語", "file": "jp_jp.json", "direction": "ltr"},
-    "ko_kr": { "name": "한국어", "file": "ko_kr.json", "direction": "ltr"},
-    "nl_nl": { "name": "Nederlands", "file": "nl_nl.json", "direction": "ltr"},
-    "pl_pl": { "name": "Polski", "file": "pl_pl.json", "direction": "ltr"},
-    "pt_br": { "name": "Português do Brasil", "file": "pt_br.json", "direction": "ltr"},
-    "ru_ru": { "name": "Русский", "file": "ru_ru.json", "direction": "ltr"},
-    "tr_tr": { "name": "Türkçe", "file": "tr_tr.json", "direction": "ltr"},
-    "ua_ua": { "name": "Українська", "file": "ua_ua.json", "direction": "ltr"},
-    "zh_cn": { "name": "中文", "file": "zh_cn.json", "direction": "ltr"},
-    "zh_tw": { "name": "中文(繁)", "file": "zh_tw.json", "direction": "ltr"}
 };
 
 function buf2hex(buffer) {
